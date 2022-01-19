7b1f85afad762ffe4df5af224e6928657737d052
Alan Hu <71209736+AlanLoveCod1ng@users.noreply.github.com>
2022-01-19 05:28:48 +0000

    Sync LeetCode submission - Maximum Number of Points with Cost (java)

---
+++ problems/maximum_number_of_points_with_cost/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long maxPoints(int[][] points) {
+        long [] dp = new long[points[0].length];
+        long max = 0;
+        for(int i = 0; i< points.length; i++){
+            for(int j = 0; j < dp.length; j++){
+                dp[j] += points[i][j];
+            }
+            
+            for(int j = 1; j<dp.length; j++){
+                dp[j] = Math.max(dp[j],dp[j-1]-1);
+            }
+            
+            for(int j = dp.length-2; j>=0; j--){
+                dp[j] = Math.max(dp[j],dp[j+1]-1);
+            }
+        }
+        for(long i: dp){
+            if(i>max){
+                max = i;
+            }
+        }
+        return max;
+    }
+}
