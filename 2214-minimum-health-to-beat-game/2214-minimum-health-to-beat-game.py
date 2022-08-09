9aef681cfbee9fc7de522a00ffb7a69b2436e4af
Alan Hu <71209736+AlanLoveCod1ng@users.noreply.github.com>
2022-08-09 14:32:50 -0500

    Time: 1142 ms (12.64%), Space: 27.9 MB (57.67%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
@@ -1,5 +1,9 @@
 class Solution:
     def minimumHealth(self, damage: List[int], armor: int) -> int:
-        totalDam = sum(damage)
-        highestDam = max(damage)
+        totalDam = 0
+        highestDam = 0
+        for i in range(len(damage)):
+            totalDam += damage[i]
+            highestDam = max(highestDam, damage[i])
+        
         return 1 + totalDam - highestDam + max(0, highestDam - armor)
