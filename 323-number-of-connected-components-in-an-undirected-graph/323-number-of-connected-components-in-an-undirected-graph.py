adb9eb8d979a592a052601d244e32c2c5849b4c2
Alan Hu <71209736+AlanLoveCod1ng@users.noreply.github.com>
2022-08-30 02:32:42 -0500

    Time: 160 ms (51.60%), Space: 15.6 MB (50.71%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,32 +1,30 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        hashtable = {}
-        components = n
-        for i in edges:
-            node1, node2 = i
-            if node1 in hashtable and node2 in hashtable:
-                set1 = hashtable[node1]
-                set2 = hashtable[node2]
-                if set1 != set2:
-                    for i in set2:
-                        set1.add(i)
-                        hashtable[i] = set1
-                    hashtable[node2] = set1
-                    components -= 1
-            elif node1 in hashtable and not node2 in hashtable:
-                set1 = hashtable[node1]
-                set1.add(node2)
-                hashtable[node2] = set1
-                components -= 1
-            elif node2 in hashtable and not node1 in hashtable:
-                set2 = hashtable[node2]
-                set2.add(node1)
-                hashtable[node1] = set2
-                components -= 1
+        def find(current):
+            if representative[current] == current:
+                return current
+            representative[current] = find(representative[current])
+            return representative[current]
+        def combine(comp1,comp2):
+            re1 = find(comp1)
+            re2 = find(comp2)
+            if re1 == re2:
+                return 0
             else:
-                new_set = set([node1,node2])
-                hashtable[node1] = new_set
-                hashtable[node2] = new_set
-                components -= 1
-        return components
-                
+                if size[re1] > size[re2]:
+                    size[re1] += size[re2]
+                    representative[re2] = re1
+                else:
+                    size[re2] += size[re1]
+                    representative[re1] = re2
+            return 1
+        representative = []
+        size = []
+        res = n
+        for i in range(n):
+            representative.append(i)
+            size.append(1)
+        for edge in edges:
+            v1, v2 = edge
+            res -= combine(v1,v2)
+        return res
